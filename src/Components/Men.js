7c30023d67e5b16a2c47c65d1ddb6e78a11cf714
Xhicko <otagadaisaac@gmail.com>
2024-03-07 17:31:08 +0100

    Started Creating Jewelries section Design

--- src/Components/Men.js
+++ src/Components/Men.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import './index.css'
+import Loader from "./Loader";
+import Error from "./Error";
 
 
 const Men = ()=>{
+
+   
+
    return(
     <div className="General_Wrapper_Design">
 
